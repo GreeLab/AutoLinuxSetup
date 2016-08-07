2faa00fdea7496c5523431743e51a9ba5015ff2d
Rafael Cruz <rafaelcruz@outlook.com>
2016-08-07 13:44:52 -0300

    Minecraft added

---
+++ Minecraft Install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Baixando e instalando Minecraft..."
+echo "----------------------------------"
+echo "Por favor, insira a senha para permissões SUDO..."
+echo "----------------------------------"
+sudo rm -Rf /opt/minecraft*
+sudo rm -Rf /usr/share/applications/Minecraft.desktop
+reset
+echo "Limpando instalações anteriores..."
+echo "----------------------------------"
+echo "Baixando Minecraft..."
+wget https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
+reset
+echo "Criando atalhos..."
+sudo mkdir /opt/minecraft
+sudo mv Minecraft.jar /opt/minecraft/
+echo -e '[Desktop Entry]\n Version=1.0\n Name=minecraft\n Exec=java -jar /opt/minecraft/Minecraft.jar\n Icon=/\n Type=Application\n Categories=Application' | sudo tee /usr/share/applications/Minecraft.desktop
+sudo chmod +x /usr/share/applications/Minecraft.desktop
+cp /usr/share/applications/Minecraft.desktop  ~/Área\ de\ Trabalho/
+reset
+echo "Execução terminada! Pressione ENTER tecla para sair"
+read
+
--- oh-my-zsh Install.sh
+++ oh-my-zsh Install.sh
@@ -3,13 +3,21 @@ clear
 echo "----------------------------"
 echo "------ INSTALLING ZSH ------"
 echo "----------------------------"
-sudo apt-get -y install zsh
+sudo apt-get install -y zsh
 chsh -s $(which zsh)
+sudo apt-get install -y tmux
 clear
 echo "---------------------------------"
 echo "----- INSTALLING OH MY ZSH ------"
 echo "---------------------------------"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# AUTO INSTALL POWERLINE MISSING
+# wget https://github.com/powerline/powerline/archive/develop.zip -O ~/Downloads/develop.zip
+# cd ~/Downloads/
+# unzip develop.zip
+# cd powerline-develop/
+
 echo "Processo concluído!"
 echo "Faça LOGOFF para as alterações entrarem em vigor!"
 read -n1 -r -p "Pressione uma tecla..." key
